fb4eb44ec19630b9bf93cfc373f65be1e7e4afa7
shockham <samuel.hockham@gmail.com>
2018-06-20 17:13:21 +0100

    Refactor to use render item tagging

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,27 @@ use caper::game::*;
 use caper::imgui::Ui;
 use caper::input::Key;
 use caper::mesh::gen_cube;
-use caper::types::{CameraBuilder, DefaultTag, RenderItemBuilder};
+use caper::types::{CameraBuilder, RenderItemBuilder};
 use caper::utils::handle_fp_inputs;
 
 mod terrain;
 
 use terrain::HALF_MAP_SIZE;
 
+#[derive(Clone)]
+enum Tags {
+    Terrain,
+}
+
+impl Default for Tags {
+    fn default() -> Tags {
+        Tags::Terrain
+    }
+}
+
 fn main() {
     // crate an instance of the game struct
-    let mut game = Game::<DefaultTag>::new();
+    let mut game = Game::<Tags>::new();
 
     game.cams[0].pos = (HALF_MAP_SIZE, HALF_MAP_SIZE, HALF_MAP_SIZE);
 
@@ -27,6 +38,7 @@ fn main() {
         RenderItemBuilder::default()
             .vertices(gen_cube())
             .instance_transforms(terrain::get_transforms(pseu_cam.pos))
+            .tag(Tags::Terrain)
             .build()
             .unwrap(),
     );
@@ -35,12 +47,19 @@ fn main() {
         // run the engine update
         let status = game.update(
             |_: &Ui| {},
-            |g: &mut Game<DefaultTag>| -> UpdateStatus {
+            |g: &mut Game<Tags>| -> UpdateStatus {
                 // update the first person inputs
                 handle_fp_inputs(&mut g.input, &mut pseu_cam);
                 g.cams[0].euler_rot = pseu_cam.euler_rot;
 
-                g.get_render_item(0).instance_transforms = terrain::get_transforms(pseu_cam.pos);
+                // deal with the diff render item types
+                for ri in g.render_items_iter_mut() {
+                    match ri.tag {
+                        Tags::Terrain => {
+                            ri.instance_transforms = terrain::get_transforms(pseu_cam.pos);
+                        }
+                    }
+                }
 
                 // quit
                 if g.input.keys_down.contains(&Key::Escape) {
