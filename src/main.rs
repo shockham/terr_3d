e7aefad6888309a1ac9c750c1646ab50c932733d
shockham <samuel.hockham@gmail.com>
2018-06-20 12:38:03 +0100

    Refactor terrain into separate mod and add pseu_cam

--- src/main.rs
+++ src/main.rs
@@ -5,62 +5,24 @@ use caper::game::*;
 use caper::imgui::Ui;
 use caper::input::Key;
 use caper::mesh::gen_cube;
-use caper::types::{DefaultTag, RenderItemBuilder, TransformBuilder, Transform};
+use caper::types::{CameraBuilder, DefaultTag, RenderItemBuilder};
 use caper::utils::handle_fp_inputs;
 
-use simdnoise::{get_3d_scaled_noise, NoiseType::Fbm};
-
-use std::iter;
+mod terrain;
 
 fn main() {
     // crate an instance of the game struct
     let mut game = Game::<DefaultTag>::new();
 
+    game.cams[0].pos = (50f32, 50f32, 50f32);
 
-    const MAP_SIZE:usize = 200;
-    const MAP_SIZE_2:usize = MAP_SIZE * MAP_SIZE;
-    const MAP_SIZE_3:usize = MAP_SIZE * MAP_SIZE * MAP_SIZE;
-
-    let verts = (0..MAP_SIZE)
-        .flat_map(|x| iter::repeat(x).take(MAP_SIZE))
-        .zip((0..MAP_SIZE).cycle().take(MAP_SIZE_3))
-        .cycle()
-        .take(MAP_SIZE_3)
-        .zip((0..MAP_SIZE).flat_map(|z| iter::repeat(z).take(MAP_SIZE_2)))
-        .map(|((x, y), z)| (x as f32, y as f32, z as f32))
-        .collect::<Vec<(f32, f32, f32)>>();
-
-    //println!("verts:{:?}", verts);
-
-    //  Set your noise type
-    let noise_type = Fbm {
-          freq: 0.04,
-          lacunarity: 0.5,
-          gain: 2.0,
-          octaves: 3,
-    };
-
-    // Get a block of 200x200x200 3d noise
-    let an_f32_vec = 
-        get_3d_scaled_noise(0.0, MAP_SIZE, 0.0, MAP_SIZE,0.0, MAP_SIZE, noise_type, 0.0, 1.0);
-
-    //println!("heights:{:?}", an_f32_vec);
-
-    let transforms = verts.iter()
-        .zip(an_f32_vec)
-        .filter(|(_, height)| *height > 0.80f32)
-        .map(|(&pos, _)| {
-            TransformBuilder::default()
-                .pos(pos)
-                .build()
-                .unwrap()
-        }).collect::<Vec<Transform>>();
+    let mut pseu_cam = CameraBuilder::default().build().unwrap();
 
     // define some items to be rendered
     game.add_render_item(
         RenderItemBuilder::default()
             .vertices(gen_cube())
-            .instance_transforms(transforms)
+            .instance_transforms(terrain::get_transforms(pseu_cam.pos))
             .build()
             .unwrap(),
     );
@@ -71,7 +33,8 @@ fn main() {
             |_: &Ui| {},
             |g: &mut Game<DefaultTag>| -> UpdateStatus {
                 // update the first person inputs
-                handle_fp_inputs(&mut g.input, &mut g.cams[0]);
+                handle_fp_inputs(&mut g.input, &mut pseu_cam);
+                g.cams[0].euler_rot = pseu_cam.euler_rot;
 
                 // quit
                 if g.input.keys_down.contains(&Key::Escape) {
---
+++ src/terrain.rs
@@ -0,0 +1,36 @@
+use caper::types::{Transform, TransformBuilder};
+use simdnoise::{get_3d_scaled_noise, NoiseType::Fbm};
+use std::iter;
+
+const MAP_SIZE: usize = 100;
+const MAP_SIZE_2: usize = MAP_SIZE * MAP_SIZE;
+const MAP_SIZE_3: usize = MAP_SIZE * MAP_SIZE * MAP_SIZE;
+
+pub fn get_transforms(pos: (f32, f32, f32)) -> Vec<Transform> {
+    let verts = (0..MAP_SIZE)
+        .flat_map(|x| iter::repeat(x).take(MAP_SIZE))
+        .zip((0..MAP_SIZE).cycle().take(MAP_SIZE_3))
+        .cycle()
+        .take(MAP_SIZE_3)
+        .zip((0..MAP_SIZE).flat_map(|z| iter::repeat(z).take(MAP_SIZE_2)))
+        .map(|((x, y), z)| (x as f32, y as f32, z as f32))
+        .collect::<Vec<(f32, f32, f32)>>();
+
+    let noise_type = Fbm {
+        freq: 0.04,
+        lacunarity: 0.5,
+        gain: 2.0,
+        octaves: 1,
+    };
+
+    let an_f32_vec = get_3d_scaled_noise(
+        pos.0, MAP_SIZE, pos.1, MAP_SIZE, pos.2, MAP_SIZE, noise_type, 0.0, 1.0,
+    );
+
+    verts
+        .iter()
+        .zip(an_f32_vec)
+        .filter(|(_, height)| *height > 0.80f32)
+        .map(|(&pos, _)| TransformBuilder::default().pos(pos).build().unwrap())
+        .collect::<Vec<Transform>>()
+}
