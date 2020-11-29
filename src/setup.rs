4aea316bdd5f048032e2cbe478a350054142df33
shockham <samuel.hockham@gmail.com>
2020-11-29 14:57:16 +0000

    Add initial textured hud

--- src/setup.rs
+++ src/setup.rs
@@ -1,9 +1,10 @@
 use super::Tags;
 use caper::game::*;
-use caper::mesh::gen_cube;
-use caper::types::{MaterialBuilder, RenderItemBuilder, TextItemBuilder};
+use caper::mesh::{gen_cube, gen_quad};
+use caper::types::{MaterialBuilder, RenderItemBuilder, TextItemBuilder, TransformBuilder};
 use caper::posteffect::PostShaderOptionsBuilder;
 use caper::utils::create_skydome;
+use caper::load_texture;
 use state::State;
 use terrain;
 use terrain::{HALF_MAP_SIZE, MAP_SIZE, SCALE};
@@ -23,6 +24,11 @@ impl Setup for Game<Tags> {
     fn setup(&mut self) -> Self::S {
         // add some shaders
         add_game_shaders(&self.renderer.display, &mut self.renderer.shaders);
+        // add some textures
+        self.renderer.shaders.textures.insert(
+            "hud",
+            load_texture!("../assets/hud.png", &self.renderer.display),
+        );
         // create init state
         let state = Self::S::default();
         // set the cam pos
@@ -49,6 +55,27 @@ impl Setup for Game<Tags> {
                 .unwrap(),
         );
         self.add_render_item(create_skydome("skydome"));
+        self.add_render_item(
+            RenderItemBuilder::default()
+                .name("hud")
+                .vertices(gen_quad())
+                .instance_transforms(vec![
+                    TransformBuilder::default()
+                        .pos((HALF_MAP_SIZE, HALF_MAP_SIZE, self.cams[0].pos.2 - 0.4f32))
+                        .scale((-1.6f32, 0.9f32, 1f32))
+                        .build()
+                        .unwrap()
+                ])
+                .material(
+                    MaterialBuilder::default()
+                        .shader_name("texture")
+                        .texture_name(Some("hud".into()))
+                        .build()
+                        .unwrap(),
+                )
+                .build()
+                .unwrap(),
+        );
 
         self.add_text_item(
             TextItemBuilder::default()
